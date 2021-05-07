da30d8983a97e7a1417ebc3a09f07edd7ea809a0
litao <ali_bojue@163.com>
2021-05-07 17:26:21 +0800

    百度统计

--- src/main.js
+++ src/main.js
@@ -26,6 +26,22 @@ Vue.use(Meta)
 Vue.use(ElementUI, {
     size: 'small'
 });
+router.afterEach((to, from, next) => {
+    setTimeout(() => {
+        //百度统计
+        var _hmt = _hmt || [];
+        (function () {
+            console.log(document.getElementById('baidu_tj'))
+            //每次执行前，先移除上次插入的代码
+            document.getElementById('baidu_tj') && document.getElementById('baidu_tj').remove();
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?a12768aeadf95daef1bf330ea8d808f9";
+            hm.id = "baidu_tj"
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    }, 0);
+});
 
 new Vue({
     router,
